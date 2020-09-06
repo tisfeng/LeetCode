bebd82609c2fbb2a2bb83e31020e2814ef1e21e2
isfeng <tisfeng@gmail.com>
2020-09-06 16:03:37 +0800

    Initial Commit

---
+++ leetcode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  leetcode
+//
+//  Created by isfeng on 2020/9/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ leetcode/leetcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  leetcodeApp.swift
+//  leetcode
+//
+//  Created by isfeng on 2020/9/6.
+//
+
+import SwiftUI
+
+@main
+struct leetcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
