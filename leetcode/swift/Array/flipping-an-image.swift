eb73c09505eadf269b70dcdfa450c5dae121243e
tisfeng <tisfeng@gmail.com>
2020-09-09 23:44:49 +0800

    update flipping-an-image

--- leetcode/swift/Array/flipping-an-image.swift
+++ leetcode/swift/Array/flipping-an-image.swift
@@ -38,3 +38,30 @@ class FlipAndInvertImage {
         }
     }
 }
+
+/**
+ 执行用时：32 ms, 在所有 Swift 提交中击败了100.00%的用户
+ 内存消耗：13.4 MB, 在所有 Swift 提交中击败了100.00%的用户
+*/
+class FlipAndInvertImage_100 {
+    func flipAndInvertImage(_ A: [[Int]]) -> [[Int]] {
+        var newA = [[Int]]()
+        for i in 0 ..< A.count {
+            var arr = A[i]
+            flipInvert(&arr)
+            newA.append(arr)
+        }
+        return newA
+    }
+    
+    func flipInvert(_ array: inout [Int]) { // [1,0,0,1,0]
+        let len = array.count
+        for i in 0 ..< len / 2 {
+            array.swapAt(i, len - i - 1)
+            array[i] == 0 ? (array[i] = 1) : (array[i] = 0)
+        }
+        for i in len / 2 ..< len {
+            array[i] == 0 ? (array[i] = 1) : (array[i] = 0)
+        }
+    }
+}
