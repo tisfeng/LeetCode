d0afb89cef4cafad7d2142c811ca1f5b7fb2299a
tisfeng <tisfeng@gmail.com>
2020-09-10 23:41:31 +0800

    create magic-squares-in-grid

---
+++ leetcode/swift/Array/magic-squares-in-grid.swift
@@ -0,0 +1,16 @@
+//
+//  magic-squares-in-grid.swift
+//  leetcode
+//
+//  Created by isfeng on 2020/9/10.
+//
+
+import Foundation
+
+class Solution {
+    func numMagicSquaresInside(_ grid: [[Int]]) -> Int {
+        var count = 0
+        
+        return count
+    }
+}
