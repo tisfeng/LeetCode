eddfcc730e2032a73f046578117b73242ef859a9
tisfeng <tisfeng@gmail.com>
2020-09-11 23:50:41 +0800

    update magic-squares-in-grid

--- leetcode/swift/Array/magic-squares-in-grid.swift
+++ leetcode/swift/Array/magic-squares-in-grid.swift
@@ -10,6 +10,36 @@ import Foundation
 class Solution {
     func numMagicSquaresInside(_ grid: [[Int]]) -> Int {
         var count = 0
+        let i_len = grid.count
+        let j_len = grid[0].count
+        
+        for i in 0 ..< i_len - 2 {
+            for j in 0 ..< j_len - 2 {
+                let row1 = grid[i][j] + grid[i][j+1] + grid[i][j+2]
+                let row2 = grid[i+1][j] + grid[i+1][j+1] + grid[i+1][j+2]
+                let row3 = grid[i+2][j] + grid[i+2][j+1] + grid[i+2][j+2]
+                
+                let column1 = grid[i][j] + grid[i+1][j] + grid[i+2][j]
+                let column2 = grid[i][j+1] + grid[i+1][j+1] + grid[i+2][j+1]
+                let column3 = grid[i][j+2] + grid[i+1][j+2] + grid[i+2][j+2]
+                
+                let slash1 = grid[i][j] + grid[i+1][j+1] + grid[i+2][j+2]
+                let slash2 = grid[i+2][j] + grid[i+1][j+1] + grid[i][j+2]
+                
+                let equal =
+                    row1 == row2 &&
+                    row1 == row3 &&
+                    row1 == column1 &&
+                    row1 == column2 &&
+                    row1 == column3 &&
+                    row1 == slash1 &&
+                    row1 == slash2
+                
+                if equal {
+                    count += 1
+                }
+            }
+        }
         
         return count
     }
