0d700cee58bb4aaece4425e524f3376a91d3781d
tisfeng <tisfeng@gmail.com>
2020-09-12 16:37:37 +0800

    beat 100% magic-squares-in-grid

--- leetcode/swift/Array/magic-squares-in-grid.swift
+++ leetcode/swift/Array/magic-squares-in-grid.swift
@@ -10,30 +10,26 @@ import Foundation
 /**
  执行用时：8 ms, 在所有 Swift 提交中击败了100.00%的用户
  内存消耗：13.7 MB, 在所有 Swift 提交中击败了100.00%的用户
-*/
+ */
 class Solution {
     func numMagicSquaresInside(_ grid: [[Int]]) -> Int {
         var count = 0
-        let i_len = grid.count
-        let j_len = grid[0].count
-        if i_len < 3 || j_len < 3 {
+        let len = 3
+        let i_len = grid.count, j_len = grid[0].count
+        if i_len < len || j_len < len {
             return 0
         }
-        for i in 0 ..< i_len - 2 {
-            for j in 0 ..< j_len - 2 {
-                let len = 3
-                var row_sum = 0, column_sum = 0
+        for i in 0 ... i_len - len {
+            for j in 0 ... j_len - len {
+                var row_sum = 0, column_sum = 0, slash_sum1 = 0, slash_sum2 = 0
                 let targetSet: Set = [1,2,3,4,5,6,7,8,9]
                 var currentSet = Set<Int>()
-                var slash_sum1 = 0, slash_sum2 = 0
                 var equal = false
                 
                 for k in 0 ..< len {
                     slash_sum1 += grid[i+k][j+k]
                     slash_sum2 += grid[i+len-k-1][j+k]
                 }
-                print("slash_sum1: \(slash_sum1)")
-                print("slash_sum2: \(slash_sum2)")
                 if slash_sum1 != slash_sum2 {
                     continue
                 }
@@ -42,7 +38,7 @@ class Solution {
                     var new_row_sum = 0, new_column_sum = 0
                     for column in 0 ..< len {
                         let num = grid[i+row][j+column]
-                        if num > 9 {
+                        if num < 1 || num > 9 {
                             continue
                         }
                         currentSet.insert(num)
@@ -56,21 +52,19 @@ class Solution {
                         column_sum = new_column_sum
                     }
                     
-                    print("new_row_sum: \(new_row_sum)")
-                    print("new_column_sum: \(new_column_sum)")
                     if row_sum == new_row_sum  &&
                         column_sum == new_column_sum &&
                         row_sum == column_sum {
                         equal = true
                     } else {
                         equal = false
+                        break
                     }
                 }
                 
-                if currentSet == targetSet && equal {
+                if equal && currentSet == targetSet {
                     count += 1
                 }
-               
             }
         }
         
@@ -104,8 +98,8 @@ class Solution_100 {
                 let slash2 = grid[i+2][j] + grid[i+1][j+1] + grid[i][j+2]
                 
                 let currentSet: Set = [grid[i][j], grid[i][j+1],grid[i][j+2],
-                                grid[i+1][j], grid[i+1][j+1],grid[i+1][j+2],
-                                grid[i+2][j], grid[i+2][j+1], grid[i+2][j+2]]
+                                       grid[i+1][j], grid[i+1][j+1],grid[i+1][j+2],
+                                       grid[i+2][j], grid[i+2][j+1], grid[i+2][j+2]]
                 let targetSet: Set = [1,2,3,4,5,6,7,8,9]
                 
                 let equal =
