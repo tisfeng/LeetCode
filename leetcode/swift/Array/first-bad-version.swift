41a230fa6ed254602d856b6b7c27926943b8cde8
tisfeng <tisfeng@gmail.com>
2020-09-13 11:14:30 +0800

    beat 98%  first-bad-version

--- leetcode/swift/Array/first-bad-version.swift
+++ leetcode/swift/Array/first-bad-version.swift
@@ -18,9 +18,30 @@ class VersionControl {
     }
 }
 
+/**
+执行用时：24 ms, 在所有 Swift 提交中击败了98.51%的用户
+内存消耗：13.1 MB, 在所有 Swift 提交中击败了100.00%的用户
+*/
 class Solution : VersionControl {
-    func firstBadVersion(_ n: Int) -> Int {
+    func firstBadVersion(_ n: Int) -> Int { //[1,2,3,4,5,6,7,8,9]
+        var start = 0
+        var end = n
+        var mid: Int
+        var firstBad = start
         
-        return 0
+        if isBadVersion(start) {
+            return firstBad
+        }
+        while end - start > 1 {
+            mid = (start + end) / 2
+            if isBadVersion(mid) {
+                end = mid
+            } else {
+                start = mid
+            }
+        }
+        firstBad = end
+        
+        return firstBad
     }
 }
