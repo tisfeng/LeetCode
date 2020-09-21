3c2b7dbc7f074f20f2c6066ecbeb228e10da6552
tisfeng <tisfeng@gmail.com>
2020-09-21 21:28:02 +0800

    create maximum-subarray

--- leetcode/swift/Array/maximum-subarray.swift
+++ leetcode/swift/Array/maximum-subarray.swift
@@ -8,8 +8,34 @@
 import Foundation
 
 class Solution {
-    func maxSubArray(_ nums: [Int]) -> Int {
+    func maxSubArray(_ nums: [Int]) -> Int { //[-2,1,-3,4,-1,2,1,-5,4]
+        let len = nums.count
+        var maxSum = Int.min
+        var currentSum = nums[0]
+        var preSum = nums[0]
+       
+        for i in 0 ..< len {
+            print("i=\(i), maxSum=\(maxSum), currentSum=\(currentSum)")
+            
+            preSum = currentSum
+            
+            currentSum += nums[i]
+            
+            if preSum < 0 {
+                currentSum = nums[i]
+                
+                if nums[i] > maxSum {
+                    maxSum = nums[i]
+                }
+                continue
+            }
+            
+            let newMaxSum = currentSum + nums[i]
+            if newMaxSum > maxSum {
+                maxSum = newMaxSum
+            }
+        }
         
-        return 0
+        return maxSum
     }
 }
