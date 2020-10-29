1de8c94596386c1169f035edf27303f598638298
tisfeng <tisfeng@gmail.com>
2020-10-29 23:54:24 +0800

    update climbing-stairs.swift

--- leetcode/swift/Dynamic-Programming/climbing-stairs.swift
+++ leetcode/swift/Dynamic-Programming/climbing-stairs.swift
@@ -7,20 +7,52 @@
 
 import Foundation
 
+/**
+ 递归，数组保存
+ 
+ 执行用时：4 ms, 在所有 Swift 提交中击败了75.39%的用户
+ 内存消耗：13.4 MB, 在所有 Swift 提交中击败了56.93%的用户
+ */
 class ClimbStairs {
-    var arr = [Int: Int]()
+    var arr = Array(repeating: 0, count: 100)
     func climbStairs(_ n: Int) -> Int {
         if n < 3 {
             arr[n] = n
             return n
         }
         
-        if let count = arr[n] {
+        if arr[n] != 0 {
+            return arr[n]
+        }
+        
+        let count = climbStairs(n - 1) + climbStairs(n - 2)
+        arr[n] = count
+        print("arr[\(n)] = \(arr[n])")
+        return count
+    }
+}
+
+/**
+ 递归，字典保存
+
+ 执行用时：4 ms, 在所有 Swift 提交中击败了75.39%的用户
+ 内存消耗：13.5 MB, 在所有 Swift 提交中击败了56.93%的用户
+ */
+class ClimbStairs_75 {
+    var map = [Int: Int]()
+    func climbStairs(_ n: Int) -> Int {
+        if n < 3 {
+            map[n] = n
+            return n
+        }
+        
+        if let count = map[n] {
             return count
         }
         
-        arr[n] = climbStairs(n - 1) + climbStairs(n - 2)
-        print("arr[\(n)] = \(arr[n]!)")
-        return arr[n]!
+        let count = climbStairs(n - 1) + climbStairs(n - 2)
+        map[n] = count
+        print("map[\(n)] = \(map[n]!)")
+        return count
     }
 }
