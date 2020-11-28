6a58bc435801f0bb0c6d821579c63b400b74249d
tisfeng <tisfeng@gmail.com>
2020-11-28 23:47:31 +0800

    beat 100%, optimized

--- leetcode/swift/Array/maximize-distance-to-closest-person.swift
+++ leetcode/swift/Array/maximize-distance-to-closest-person.swift
@@ -16,12 +16,26 @@ import Foundation
  */
 
 /**
- 执行用时：100 ms, 在所有 Swift 提交中击败了100.00%的用户
+ 执行用时：96 ms, 在所有 Swift 提交中击败了100.00%的用户
  内存消耗：14.2 MB, 在所有 Swift 提交中击败了100.00%的用户
  */
 class Solution {
     func maxDistToClosest(_ seats: [Int]) -> Int {
         let len = seats.count
+        var count = 0
+        // 末尾优化 [0,1,0,0]
+        for num in seats.reversed() {
+            if num == 0 {
+                count += 1
+            } else {
+                if count >= len / 2 {
+//                    print("last optimize")
+                    return count
+                }
+                break;
+            }
+        }
+        
         // 记录连续0的位置，arr[1]=3 表示seats[1]位置起有3个连续0
         // arr[0]记录起始边界，arr[1]记录中间位置，arr[2]记录末尾边界
         var arr = [Int](repeating: 0, count: 3)
@@ -33,6 +47,12 @@ class Solution {
             while index < len && seats[index] == 0 {
                 index += 1
                 count += 1
+            }
+            
+            // 起点优化 [0,0,1,0,0]
+            if start == 0 && count >= len / 2 {
+//                print("first optimize")
+                return count
             }
             
             if count > 0 {
@@ -52,7 +72,7 @@ class Solution {
         
         let midMax = (arr[1] + 1) / 2
         let boundMax = max(arr[0], arr[2])
-        print("arr: \(arr), mid: \(boundMax)")
+//        print("arr: \(arr), mid: \(boundMax)")
         
         return max(midMax, boundMax)
     }
