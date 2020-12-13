69f09316091ec01990b2f6c736efc966151eb3c4
tisfeng <tisfeng@gmail.com>
2020-12-13 19:04:11 +0800

    try commit

--- leetcode/swift/Array/diagonal-traverse-ii.swift
+++ leetcode/swift/Array/diagonal-traverse-ii.swift
@@ -19,11 +19,12 @@ import Foundation
  
  输出：[1,6,2,8,7,3,9,4,12,10,5,13,11,14,15,16]
  
- 0.0
- 1.0 0.1
- 2.0 1.1 0.2
- 2.1 1.2
- 2.2
+ [
+ [1,2,3],
+ [4],
+ [5,6,7],
+ [8],
+ [9,10,11]]
 
  */
 class Solution {
@@ -32,10 +33,7 @@ class Solution {
         let firstRowCount = nums.count
         let firstColumnCount = nums[0].count
         let lastColumCount = nums[firstRowCount-1].count
-//        let diagonalRow = max(firstRowCount, firstColumnCount) + lastColumCount - 1
-        
-        print("firstRowCount: \(firstRowCount), firstColumnCount: \(firstColumnCount)")
-        
+//        print("\(firstRowCount), \(lastColumCount) firstColumnCount: \(firstColumnCount)")
         // 打印左上区域
         for index in 0..<firstRowCount-1 {
             var i = index
@@ -43,30 +41,41 @@ class Solution {
             while j <= index {
                 if j < nums[i].count {
                     let a = nums[i][j]
-                    print("num[\(i)][\(j)]=\(a)")
+//                    print("num[\(i)][\(j)]=\(a)")
                     arr.append(a)
                 }
                 j += 1
                 i -= 1
             }
         }
+//        print("\narr: \(arr)")
+
+        var diagonalCount = lastColumCount
         
-        print("\narr: \(arr)")
+        if firstRowCount + lastColumCount < firstColumnCount {
+            diagonalCount = firstColumnCount - firstRowCount
+        }
+        
+        let maxRow = max(firstColumnCount, lastColumCount)
+        
+//        print("diagonalCount=\(diagonalCount), maxRow=\(maxRow)")
         
         // 打印右下区域 [2][0],[1][1],[0][2],  [2][1],[1][2]  [2,2]
-        for index in 0..<lastColumCount { // 2,1,0
+        for index in 0..<diagonalCount { // 2,1,0
         
-            var i = lastColumCount - 1
+            var i = firstRowCount - 1
             var j = index
-            while j < lastColumCount {
+            
+            print("i=\(i), j=\(j), count=\(nums[i].count)")
+            while i >= 0 && j < maxRow {
                 if j < nums[i].count {
                     let a = nums[i][j]
-                    print("num[\(i)][\(j)]=\(a)")
+                    print("push: num=\(a)")
                     arr.append(a)
                 }
-                
                 j += 1
                 i -= 1
+                print("--> i=\(i), j=\(j)") // count=\(nums[i].count)
             }
         }
         
