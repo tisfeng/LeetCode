c3b66a06e31d606080b582b502283377d36ac3bd
tisfeng <tisfeng@gmail.com>
2020-12-13 21:08:04 +0800

    beat 50%

--- leetcode/swift/Array/diagonal-traverse-ii.swift
+++ leetcode/swift/Array/diagonal-traverse-ii.swift
@@ -35,7 +35,7 @@ import Foundation
 
  超时
  */
-class Solution {
+class FindDiagonalOrder_0 {
     func findDiagonalOrder(_ nums: [[Int]]) -> [Int] {
         var arr = [Int]()
         let firstRowCount = nums.count
@@ -85,5 +85,36 @@ class Solution {
         }
         
         return arr
+    }
+}
+
+/**
+ 执行用时：1440 ms, 在所有 Swift 提交中击败了50.00%的用户
+ 内存消耗：24.9 MB, 在所有 Swift 提交中击败了100.00%的用户
+ */
+class Solution {
+    typealias NumberInfo = (r:Int,c:Int,val:Int)
+    func findDiagonalOrder(_ nums: [[Int]]) -> [Int] {
+        guard nums.count > 1 else {
+            return nums[0]
+        }
+        var numbers = [NumberInfo]()
+        for r in 0..<nums.count {
+            for c in 0..<nums[r].count {
+                numbers.append((r,c,nums[r][c]))
+            }
+        }
+        let sortedNumbers = numbers.sorted { (arg0, arg1) -> Bool in
+            
+            let (r, c, _) = arg0
+            let (r1, c1, _) = arg1
+            
+            if r + c == r1 + c1 {
+                return r > r1
+            }
+            return r + c < r1 + c1
+        }
+        
+        return sortedNumbers.map {$0.val}
     }
 }
