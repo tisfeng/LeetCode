001c268b08f874a2e453b0751872ba944dcfdbd6
tisfeng <tisfeng@gmail.com>
2020-12-13 20:56:39 +0800

    over time, diagonal-traverse-ii

--- leetcode/swift/Array/diagonal-traverse-ii.swift
+++ leetcode/swift/Array/diagonal-traverse-ii.swift
@@ -33,21 +33,25 @@ import Foundation
  [13,14,15,8,11],
  [11,13,1]]
 
+ 超时
  */
 class Solution {
     func findDiagonalOrder(_ nums: [[Int]]) -> [Int] {
         var arr = [Int]()
         let firstRowCount = nums.count
-        let firstColumnCount = nums[0].count
         let lastColumCount = nums[firstRowCount-1].count
         
         var maxRow = 0
-        var maxRowIndex = 0
+        var maxDiagonalCount = 0
         // 打印左上区域
         for index in 0..<firstRowCount-1 {
-            if nums[index].count >= maxRow {
+            let diagonalCount = nums[index].count - (firstRowCount - index - 1)
+            if diagonalCount >= maxDiagonalCount {
+                maxDiagonalCount = diagonalCount
+            }
+            
+            if nums[index].count > maxRow {
                 maxRow = nums[index].count
-                maxRowIndex = index
             }
             
             var i = index
@@ -64,29 +68,13 @@ class Solution {
         }
 //        print("\narr: \(arr)")
 
-        
-        var diagonalCount = lastColumCount
-        
-//        if firstRowCount + lastColumCount < maxRow {
-//            diagonalCount = maxRow - firstRowCount
-//        }
-        
-        diagonalCount = maxRow - (firstRowCount - maxRowIndex - 1) // 7-5-1
-        
-        
-        print("\(maxRow), \(diagonalCount)")
+        maxDiagonalCount = max(maxDiagonalCount, lastColumCount)
+        maxRow = max(maxRow, lastColumCount)
+        //        print("diagonalCount=\(maxDiagonalCount), maxRow=\(maxRow)")
 
-//        if maxRow - maxRowIndex > lastColumCount {
-//            diagonalCount = maxRow - maxRowIndex
-//        }
-        
-        diagonalCount = max(diagonalCount, lastColumCount)
-        print("diagonalCount=\(diagonalCount), maxRow=\(maxRow)")
-        
-        maxRow = max(maxRow, diagonalCount)
         
         // 打印右下区域 [2][0],[1][1],[0][2],  [2][1],[1][2]  [2,2]
-        for index in 0..<diagonalCount { // 2,1,0
+        for index in 0..<maxDiagonalCount { // 2,1,0
             var i = firstRowCount - 1
             var j = index
             
@@ -105,13 +93,15 @@ class Solution {
    
         /**
          [
-         [1,36,9,7,4,20,1,7],
-         [11,27,7,38,32,17,13],
-         [35,16,7,7,21,13],
-         [5,40,27,37,26],
-         [12,17,2,3,17,9,6,4],
-         [29,5,19,37,4,7,34,32,9],
-         [13,34,20,24,32]]
+         [35,20,24,2,14,16,20],
+         [27,25,2,36,23,31,10,1],
+         [31,23,8,26,27,16],
+         [26,30,35,17,13,12,12,14,34,20],
+         [30,20,36,27,1,2,27],
+         [32,24,2,29,3],
+         [39,14,28,8,8,9,29,30,20],
+         [30,13,39,16,8,21,2,5,3],
+         [7,14,28,18,3]]
          */
         
 //        print("\narr: \(arr)")
