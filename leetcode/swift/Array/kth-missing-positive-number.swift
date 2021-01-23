50abe3950fb011a5fdc13a7228585a726219e61c
tisfeng <tisfeng@gmail.com>
2021-01-23 10:23:15 +0800

    create kth-missing-positive-number.swift

---
+++ leetcode/swift/Array/kth-missing-positive-number.swift
@@ -0,0 +1,22 @@
+//
+//  kth-missing-positive-number.swift
+//  leetcode
+//
+//  Created by tisfeng on 2021/1/23.
+//
+
+import Foundation
+
+/**
+ 1539. 第 k 个缺失的正整数 https://leetcode-cn.com/problems/kth-missing-positive-number/
+ 
+ 输入：arr = [2,3,4,7,11], k = 5
+ 输出：9
+ 解释：缺失的正整数包括 [1,5,6,8,9,10,12,13,...] 。第 5 个缺失的正整数为 9 。
+ */
+class FindKthPositive {
+    func findKthPositive(_ arr: [Int], _ k: Int) -> Int {
+
+        return 0
+    }
+}
